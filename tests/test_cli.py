c9d9ad9c83c87fdb9503dd64a69688bc68f9f972
Thiago Paes <mrprompt@gmail.com>
2020-09-06 04:08:43 -0300

    fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,6 @@ from stacker.scripts.cli import cli
 
 def test_cli_count():
     runner = CliRunner()
-    result = runner.invoke(cli, ['./', 'output.jpg'])
+    result = runner.invoke(cli, ['./', '*P.jpg', 'output.jpg'])
     assert result.exit_code == 0
     assert result.output == "- Reading captures\n- Nothing to do\n"
